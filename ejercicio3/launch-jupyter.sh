1dd1872212942f340749a7a8b247d290377c242a
Alan Dominguez <edgar@neo.localdomain>
2025-06-13 09:00:16 -0600

    Ajustes menores

--- ejercicio3/launch-jupyter.sh
+++ ejercicio3/launch-jupyter.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker run --rm -p 8888:8888 \
   -v "$(pwd)/notebooks:/home/jovyan/work" \
-  jupyter/base-notebook
+  jupyter/base-notebook start-notebook.sh --NotebookApp.token='' --NotebookApp.password=''
